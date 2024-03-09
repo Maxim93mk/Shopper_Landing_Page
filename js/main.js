ae6ca2c732e0c24e2c399f576fce8cfd324ba407
Maxim93mk <kinjashov.m@rambler.ru>
2024-03-09 21:02:52 +0300

    Мобильная версия, добавлены переходы

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const mobile_menu = document.querySelector('.mobile-menu');
+const mobile_links_nav = document.querySelector('.mobile-menu__navigation');
+
 
 
 document.querySelector('.header-logo-form__menu_button')
@@ -9,6 +11,30 @@ document.querySelector('.header-logo-form__menu_button')
 
 
 mobile_menu.querySelector('.mobile-menu__close')
+    .addEventListener('click', (event) => {
+        mobile_menu.classList.add('hidden');
+        mobile_menu.classList.remove('visible');
+    })
+
+mobile_links_nav.querySelector('.link1')
+    .addEventListener('click', (event) => {
+        mobile_menu.classList.add('hidden');
+        mobile_menu.classList.remove('visible');
+    })
+
+mobile_links_nav.querySelector('.link2')
+    .addEventListener('click', (event) => {
+        mobile_menu.classList.add('hidden');
+        mobile_menu.classList.remove('visible');
+    })
+
+mobile_links_nav.querySelector('.link3')
+    .addEventListener('click', (event) => {
+        mobile_menu.classList.add('hidden');
+        mobile_menu.classList.remove('visible');
+    })
+
+mobile_links_nav.querySelector('.link4')
     .addEventListener('click', (event) => {
         mobile_menu.classList.add('hidden');
         mobile_menu.classList.remove('visible');
