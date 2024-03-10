61f2ace500f408ea1d58346dc7449915a5884627
Maxim93mk <kinjashov.m@rambler.ru>
2024-03-10 14:54:23 +0300

    Добавлены переключения кнопок и добавление новых товаров

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,10 @@
-const products_container = document.querySelector('.products-container');
+const products_containerS = document.querySelector('.section-top-sellers').querySelector('.products-container');
+const products_containerT = document.querySelector('.section-trending-earphones').querySelector('.products-container');
 
-const productsInfoList = [{
+const top_sellers_btn = document.querySelector('.section-top-sellers').querySelector('.section-top-sellers__btns');
+const trending_earphones_btn = document.querySelector('.section-trending-earphones').querySelector('.section-top-sellers__btns');
+
+const productsInfoListPick = [{
     productName: "Наушники 1",
     productReviews: 155,
     productPrice: 145,
@@ -13,6 +17,46 @@ const productsInfoList = [{
     productReviews: 999,
     productPrice: 14,
 }];
+
+const productsInfoListWatches = [{
+    productName: "Наушники 1w",
+    productReviews: 5,
+    productPrice: 14,
+}, {
+    productName: "Наушники 2w",
+    productReviews: 1,
+    productPrice: 1550,
+}, {
+    productName: "Наушники 3w",
+    productReviews: 9999,
+    productPrice: 3,
+}];
+
+
+
+top_sellers_btn.addEventListener('click', (event) => {
+    const circBtns = top_sellers_btn.querySelectorAll('button');
+    const target = event.target;
+    circBtns.forEach(btn => {
+        btn.classList.remove('active');
+
+    });
+    target.classList.add('active');
+    getProduct(productsInfoListPick, products_containerS);
+});
+
+trending_earphones_btn.addEventListener('click', (event) => {
+    const circBtns = trending_earphones_btn.querySelectorAll('button');
+    const target = event.target;
+
+    circBtns.forEach(btn => {
+        btn.classList.remove('active');
+    });
+    target.classList.add('active');
+    getProduct(productsInfoListWatches, products_containerT);
+});
+
+
 
 function getProduct(productsInfoList, elem) {
     const arr = productsInfoList.forEach(productInfo => {
@@ -43,8 +87,4 @@ function getProduct(productsInfoList, elem) {
         elem.insertAdjacentHTML("beforeEnd", products_main);
     })
 }
-
-
-getProduct(productsInfoList, products_container);
-
 
